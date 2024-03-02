3912a9469410954e506949673ecc08935ed17ecb
HomagoDon <miguelsalazardebarros@gmail.com>
2024-03-02 11:56:16 -0300

    tic_tac_toe

---
+++ tic.tac.py
@@ -0,0 +1,11 @@
+game_win = ()
+
+
+Player = 0
+test = {1: '#|#|#', 2: '#|#|#', 3: '#|#|#'}
+ls_test = list(test.values())
+a = ls_test[0][0] 
+
+print(a)
+print(f'{ls_test[0]}\n{ls_test[1]}\n{ls_test[2]} ')
+
